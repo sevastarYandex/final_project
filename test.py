a08209d0fd6309005d401f6c5e5c088eab00f22a
sevastarYandex <sevastar1475@gmail.com>
2023-04-10 02:11:01 +0400

    word_resource testing

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ def mpt(par, fun, params=None, if_p=True):
     print('-' * 30)
 
 
-if __name__ == "__main__":
+def user_api():
     mpt('/api/user', get)
     mpt('/api/user/1', get)
     mpt('/api/user/2', get)
@@ -50,3 +50,61 @@ if __name__ == "__main__":
     mpt('/api/user', get)
     mpt('/api/user/2', delete)
     mpt('/api/user', get)
+
+
+def word_api():
+    mpt('/api/word', get)
+    mpt('/api/word/1', get)
+    mpt('/api/word/2', get)
+    mpt('/api/word/dhjsvdn', get)
+    mpt('/api/word', post, {'word': 'sm_word',
+                            'translation_list': 'какое-то слово',
+                            'user_id': 2,
+                            'is_public': True})
+    mpt('/api/word', get)
+    mpt('/api/word', post, {'word': 'word',
+                            'translation_list': 'слово',
+                            'user_id': 1,
+                            'is_public': True})
+    mpt('/api/word', get)
+    mpt('/api/word', post, {'word': 'not a word',
+                            'translation_list': 'не слово',
+                            'user_id': 1,
+                            'is_public': True})
+    mpt('/api/word', get)
+    mpt('/api/word/sfsdf', put, {'word': 'not a word',
+                            'translation_list': 'не слово',
+                            'user_id': 1,
+                            'is_public': True})
+    mpt('/api/word', get)
+    mpt('/api/word/3', put, {'word': 'not a word',
+                            'translation_list': 'не слово',
+                            'user_id': 1,
+                            'is_public': True})
+    mpt('/api/word', get)
+    mpt('/api/word/2', put, {'word': 'not a word',
+                            'translation_list': 'не слово',
+                            'user_id': 2,
+                            'is_public': True})
+    mpt('/api/word', get)
+    mpt('/api/word/2', put, {'word': 'a word',
+                            'translation_list': 'не слово',
+                            'user_id': 1,
+                            'is_public': True})
+    mpt('/api/word', get)
+    mpt('/api/word/2', put, {'word': 'not a word',
+                             'translation_list': 'не слово',
+                             'user_id': 1,
+                             'is_public': False})
+    mpt('/api/word', get)
+    mpt('/api/word/3', delete)
+    mpt('/api/word', get)
+    mpt('/api/word/sdfsd', delete)
+    mpt('/api/word', get)
+    mpt('/api/word/2', delete)
+    mpt('/api/word', get)
+
+
+if __name__ == "__main__":
+    user_api()
+    word_api()
