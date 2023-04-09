c2c24b50d09623a776b53d867d564aa09ff498d6
sevastarYandex <sevastar1475@gmail.com>
2023-04-09 14:54:15 +0400

    main tables

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect
 from flask_login import login_user, logout_user, login_required, LoginManager
 from flask_restful import Api
 from data import db_session
+from data.user import User
 import os
 
 
@@ -16,6 +17,13 @@ def init_data():
     if os.path.exists(db_name):
         return
     db_session.global_init(db_name)
+    db_sess = db_session.create_session()
+    user = User()
+    user.nick = 'admin228'
+    user.email = 'sevastar1475@gmail.com'
+    user.set_password('__admin228__')
+    db_sess.add(user)
+    db_sess.commit()
 
 
 def main():
