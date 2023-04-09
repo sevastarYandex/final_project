0ad82941f5b4c6a539726bac6d0b3ca354c0ec9b
sevastarYandex <sevastar1475@gmail.com>
2023-04-09 12:57:38 +0400

    start

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, make_response, jsonify
+from flask import Flask, render_template, redirect
 from flask_login import login_user, logout_user, login_required, LoginManager
 from flask_restful import Api
 from data import db_session
@@ -12,16 +12,6 @@ login_manager.init_app(app)
 db_name = 'db/eng_db.db'
 
 
-@app.errorhandler(404)
-def not_found(_):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-
-@app.errorhandler(400)
-def bad_request(_):
-    return make_response(jsonify({'error': 'Bad Request'}), 400)
-
-
 def init_data():
     if os.path.exists(db_name):
         os.remove(db_name)
@@ -29,7 +19,11 @@ def init_data():
 
 
 def main():
-    init_data()
+    try:
+        init_data()
+    except Exception:
+        print('error: please close database in other apps')
+        exit(0)
     api = Api(app)
     app.run(port=8080, host='127.0.0.1')
 
