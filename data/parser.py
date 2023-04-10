11282f3cf0e5ac7802ad0c784c17a6369105fb34
sevastarYandex <sevastar1475@gmail.com>
2023-04-10 20:53:01 +0400

    dict_res restoring

--- data/parser.py
+++ data/parser.py
@@ -14,8 +14,11 @@ word_put_parser.add_argument('is_pb', required=True, type=bool)
 word_post_parser = word_put_parser
 word_post_parser.add_argument('user_id', required=True, type=int)
 
-dict_parser = reqparse.RequestParser()
-dict_parser.add_argument('title', required=True)
-dict_parser.add_argument('desc', required=True)
-dict_parser.add_argument('wd_ids', required=True)
-dict_parser.add_argument('is_pb', required=True, type=bool)
+dict_put_parser = reqparse.RequestParser()
+dict_put_parser.add_argument('title', required=True)
+dict_put_parser.add_argument('desc', required=True)
+dict_put_parser.add_argument('wd_ids', required=True)
+dict_put_parser.add_argument('is_pb', required=True, type=bool)
+
+dict_post_parser = dict_put_parser
+dict_post_parser.add_argument('user_id', required=True, type=int)
